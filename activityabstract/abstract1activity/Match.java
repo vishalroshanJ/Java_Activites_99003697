33e3a76f0e1c04bbf7516f4c5b5181f391b7c757
vishalroshanJ <78852893+vishalroshanJ@users.noreply.github.com>
2021-02-26 17:33:19 +0530

    Add files via upload

---
+++ activityabstract/abstract1activity/Match.java
@@ -0,0 +1,39 @@
+package interfaceacttivity;
+import java.util.*;
+public abstract class Match {
+	
+private int currentscore;
+private float currentover;
+private int  target;
+
+public Match() {
+	super();
+}
+public Match(int currentscore, float currentover, int target) {
+	super();
+	this.currentscore = currentscore;
+	this.currentover = currentover;
+	this.target = target;
+}
+public int getCurrentscore() {
+	return currentscore;
+}
+public void setCurrentscore(int currentscore) {
+	this.currentscore = currentscore;
+}
+public float getCurrentover() {
+	return currentover;
+}
+public void setCurrentover(float currentover) {
+	this.currentover = currentover;
+}
+public int getTarget() {
+	return target;
+}
+public void setTarget(int target) {
+	this.target = target;
+}
+abstract float calculaterunRate();
+abstract float calculateballs();
+abstract void display();
+}
