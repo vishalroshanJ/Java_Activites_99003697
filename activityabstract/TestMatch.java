6ee76cfc04d4e7d6cfc90183ca944ac8eadaef98
vishalroshanJ <78852893+vishalroshanJ@users.noreply.github.com>
2021-02-26 17:20:57 +0530

    Add files via upload

---
+++ activityabstract/TestMatch.java
@@ -0,0 +1,23 @@
+package interfaceacttivity;
+
+public class TestMatch extends  Match{
+
+	@Override
+	float calculaterunRate() {
+		float a=(getTarget()-getCurrentscore())/(90-getCurrentover());
+		return a;
+	}
+
+	@Override
+	float calculateballs() {
+		float b=(50-getCurrentover())*6;
+		return b;
+	}
+
+	@Override
+	void display(double reqRunrate, int balls) {
+		System.out.println("NEED"+(getTarget()-getCurrentscore())+"Runs in "+calculateballs()+"Balls");
+		System.out.println("Required Runrate:"+calculaterunRate());
+	}
+
+}
